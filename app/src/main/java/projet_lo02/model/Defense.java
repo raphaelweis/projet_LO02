2cac17674fd9e5ea2b9d748d3fbd1ad73103af25
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-01-06 11:26:12 +0100

    fixed bug in defense

--- app/src/main/java/projet_lo02/model/Defense.java
+++ app/src/main/java/projet_lo02/model/Defense.java
@@ -27,15 +27,12 @@ public class Defense implements Strategie {
         //calcul gain crédits
 
         Etudiant cible = actionneur.getZone().etudiantLePlusFaibleAllie(actionneur);
-        int gainCreditsNet = Utility.getPartieEntiere(y * (10 + cible.getConstitution()));
-        int gainCreditsMax = 30 + cible.getConstitution();
-
-        if(gainCreditsNet >= gainCreditsMax){gainCreditsNet = gainCreditsMax;}
+        int gainCreditsNet = Utility.getPartieEntiere(y * 10);
 
         //mettre à jour crédits si défense réussie
-        if(x <= 20 + 6 * actionneur.getDexterite() && gainCreditsNet != 0){ //soin réussi
+        if(x <= 20 + 6 * actionneur.getDexterite() && gainCreditsNet != 0 && cible.getCredits() > 30 + cible.getConstitution()){ //soin réussi
             cible.setCredits(cible.getCredits() + gainCreditsNet);
-            System.out.println("Le soin est réussi, "  + cible.getIdEtudiant()+ "de" + cible.getJoueur().getPseudo()+ " a été soigné de " + gainCreditsNet + " crédits");
+            System.out.println("Le soin est réussi, "  + cible.getIdEtudiant()+ " de " + cible.getJoueur().getPseudo()+ "a un nouveau solde de crédits de " + cible.getCredits() + " crédits");
         } else System.out.println("Le soin est raté, l'étudiant ciblé n'a pas été soigné.");
     }
 }
